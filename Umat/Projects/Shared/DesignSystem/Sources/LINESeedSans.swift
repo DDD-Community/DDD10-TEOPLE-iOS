4e6a315cf53784ee21d0438edc7f2e63387bdd4f
Juny0110 <wnsdyds403@naver.com>
2024-01-28 14:37:26 +0900

    Style #1: CaseIterable 프로토콜 제거

--- Umat/Projects/Shared/DesignSystem/Sources/LINESeedSans.swift
+++ Umat/Projects/Shared/DesignSystem/Sources/LINESeedSans.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum LINESeedSans: String, CaseIterable {
+enum LINESeedSans: String {
     case bold = "LINESeedSansKR-Bold"
     case regular = "LINESeedSansKR-Regular"
 }
--- Umat/Projects/Shared/DesignSystem/Sources/Pretendard.swift
+++ Umat/Projects/Shared/DesignSystem/Sources/Pretendard.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum Pretendard: String, CaseIterable {
+enum Pretendard: String {
     case bold = "Pretendard-Bold"
     case semiBold = "Pretendard-SemiBold"
     case medium = "Pretendard-Medium"
