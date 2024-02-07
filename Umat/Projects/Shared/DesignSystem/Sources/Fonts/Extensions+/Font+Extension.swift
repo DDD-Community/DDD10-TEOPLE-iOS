a82b0bfbdf3542567cb84d37452ae91935bc1893
Juny0110 <wnsdyds403@naver.com>
2024-02-07 19:17:22 +0900

    Fix #10: Font Register 메서드 추가
    - FontManager에 Font 등록하는 메서드 추가
      - fontName 또는 확장자 오류 시 print() 호출
      - 이미 등록된 폰트에 대해서는 print()호출
    - Bundle.module을 통해 연결

--- Umat/Projects/Shared/DesignSystem/Sources/Fonts/Extensions+/Font+Extension.swift
+++ Umat/Projects/Shared/DesignSystem/Sources/Fonts/Extensions+/Font+Extension.swift
@@ -18,3 +18,28 @@ extension Font {
         return .init(UIFont.lineSeedSans(font.design, size: font.size))
     }
 }
+
+// MARK: - Font Register
+public extension Font {
+    static func registerFonts() {
+        for font in Pretendard.allCases {
+            Font.registerFont(module: .module, fontName: font.rawValue)
+        }
+        
+        for font in LINESeedSans.allCases {
+            Font.registerFont(module: .module, fontName: font.rawValue)
+        }
+    }
+}
+
+extension Font {
+    static func registerFont(module: Bundle, fontName: String) {
+        guard let url = module.url(forResource: fontName, withExtension: ".otf") else {
+            return debugPrint("Font Register 실패: fontName 또는 파일 확장자를 확인하세요.")
+        }
+        
+        if !CTFontManagerRegisterFontsForURL(url as CFURL, .process, nil) {
+            return debugPrint("Font Register 실패: 이미 등록되어있는 폰트입니다.")
+        }
+    }
+}
