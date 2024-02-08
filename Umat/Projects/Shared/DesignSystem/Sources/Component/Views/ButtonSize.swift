8484b5c25e66858d03b70fb6605a2b5858cfc50d
Juny0110 <wnsdyds403@naver.com>
2024-02-09 00:58:06 +0900

    Feat #9: 버튼 사이즈 모델 추가
    - large, medium, small, xsmall 사이즈별 font, icon, inset 값 적용 모델 추가

---
+++ Umat/Projects/Shared/DesignSystem/Sources/Component/Views/ButtonSize.swift
@@ -0,0 +1,56 @@
+//
+//  ButtonSize.swift
+//  DesignSystem
+//
+//  Created by 지준용 on 2/9/24.
+//  Copyright © 2024 KYUNG MIN CHOI. All rights reserved.
+//
+
+import SwiftUI
+
+
+public enum ButtonSize {
+    case large
+    case medium // default
+    case small
+    case xsmall
+    
+    var font: Fonts.PretendardFonts {
+        switch self {
+        case .large:
+            return .ps18
+        case .medium:
+            return .ps16
+        case .small:
+            return .ps14
+        case .xsmall:
+            return .ps12
+        }
+    }
+    
+    var icon: CGFloat {
+        switch self {
+        case .large:
+            return 24
+        case .medium:
+            return 22
+        case .small:
+            return 20
+        case .xsmall:
+            return 12
+        }
+    }
+    
+    var insets: EdgeInsets {
+        switch self {
+        case .large:
+            return .init(top: 15, leading: 24, bottom: 15, trailing: 24)
+        case .medium:
+            return .init(top: 13, leading: 24, bottom: 13, trailing: 24)
+        case .small:
+            return .init(top: 10, leading: 20, bottom: 10, trailing: 20)
+        case .xsmall:
+            return .init(top: 6, leading: 12, bottom: 6, trailing: 12)
+        }
+    }
+}
