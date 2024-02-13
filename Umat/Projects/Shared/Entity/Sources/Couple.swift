87b541188251a8a41e86a8dff34423b8c6f23565
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-02-14 01:45:51 +0900

    [Feature] 마이페이지에서 사용할 모델 작성 (#18)
    * Feat #15: 개개인의 사용자를 위한 모델 작성
        - 유저 ID, 이름, 알러지 음식 품목, 생일, 가고 싶은 곳 등을 포함
        - 이름과 알러지 품목, 생일, 가고 싶은 곳을 바꾸는 메서드 구현: 접근은 public, 수정은 메서드를 통해서만
        - 알러지 품목 정의
    
    * Feat #15: 커플 모델 작성 및 모델에 관한 주석 추가
    
    * Feat #15: 장소 모델 정의
        - 마이페이지에 가고 싶은 장소를 카운팅해주는 기능이 있어서, 장소 모델을 먼저 정의
    
    * Feat #15: 장소 모델이 필요한 곳에 프로퍼티 생성하고 삽입
        - 나의 위시리스트, 우리의 위시리스트 프로퍼티 추가
        - 나의 위시리스트 수정하는 메서드 추가
        - 우리의 위시리스트 수정, 삭제하는 메서드는 향후 작성
        - 우리 위시리스트에 채워넣는 방식에 대해 논의 필요

---
+++ Umat/Projects/Shared/Entity/Sources/Couple.swift
@@ -0,0 +1,42 @@
+//
+//  Couple.swift
+//  Entity
+//
+//  Created by Gordon Choi on 2/13/24.
+//  Copyright © 2024 KYUNG MIN CHOI. All rights reserved.
+//
+
+import Foundation
+
+/// 커플을 위한 모델입니다.
+/// 나의 정보 및 기념일을 바꾸었을 때 반영할 수 있도록 var로 했습니다.
+/// id나 you의 정보는 내가 임의로 바꿀 수 없도록 let으로 지정했습니다.
+/// 이 부분은 백엔드 단에서 내려줄 때만 새로운 객체로 수정하면 됩니다.
+public struct Couple {
+    private let id: String
+    public private(set) var me: UserInfo
+    public let you: UserInfo
+    public private(set) var anniversary: Date
+    public private(set) var wishlist: [Place]
+    
+    public init(id: String, me: UserInfo, you: UserInfo, anniversary: Date, wishlist: [Place]) {
+        self.id = id
+        self.me = me
+        self.you = you
+        self.anniversary = anniversary
+        self.wishlist = wishlist
+    }
+    
+    public mutating func changeMyInfo(me newInfo: UserInfo) {
+        me = newInfo
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+    
+    public mutating func changeAnniversary(anniversary newDate: Date) {
+        anniversary = newDate
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+    
+    // TODO: "우리" 리스트에 채워넣거나 삭제하는 메서드
+    // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+}
---
+++ Umat/Projects/Shared/Entity/Sources/UserInfo.swift
@@ -0,0 +1,78 @@
+//
+//  UserInfo.swift
+//  Entity
+//
+//  Created by Gordon Choi on 2/13/24.
+//  Copyright © 2024 KYUNG MIN CHOI. All rights reserved.
+//
+
+import Foundation
+
+/// 사용자 한 명에 대한 정보입니다.
+/// id값은 바뀌면 안 되므로 let입니다.
+/// 이름이나 알러지 품목은 바뀔 수 있으므로 var로 하고, 수정 메서드를 작성했습니다.
+public struct UserInfo {
+    private let id: String
+    public private(set) var name: String
+    public private(set) var birthday: Date
+    public private(set) var allergicFoods: [AllergicFood]
+    public private(set) var wishlist: [Place]
+    
+    public init(id: String, name: String, birthday: Date, allergicFoods: [AllergicFood], wishlist: [Place]) {
+        self.id = id
+        self.name = name
+        self.birthday = birthday
+        self.allergicFoods = allergicFoods
+        self.wishlist = wishlist
+    }
+    
+    public mutating func changeName(name newName: String) {
+        name = newName
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+    
+    public mutating func changeBirthday(date newBirthday: Date) {
+        birthday = newBirthday
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+    
+    public mutating func changeAllergicFoods(foods newAllergicFoods: [AllergicFood]) {
+        allergicFoods = newAllergicFoods
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+    
+    public mutating func addPlace(place: Place) {
+        wishlist.append(place)
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+    
+    public mutating func deletePlace(place: Place) {
+        wishlist = wishlist.filter { $0 != place }
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
+    }
+}
+
+/// 식약처에서 제공하는 알러지 품목에 관한 목록입니다.
+public enum AllergicFood: String, CaseIterable {
+    case crab = "게"
+    case mackerel = "고등어"
+    case oyster = "굴"
+    case eggs = "난류"
+    case chicken = "닭고기"
+    case soybean = "대두"
+    case pork = "돼지고기"
+    case peanut = "땅콩"
+    case buckwheat = "메밀"
+    case wheat = "밀"
+    case peach = "복숭아"
+    case shrimp = "새우"
+    case beef = "쇠고기"
+    case sulfurousAcids = "아황산 포함 식품"
+    case squid = "오징어"
+    case milk = "우유"
+    case abalone = "전복"
+    case shellfishes = "조개류"
+    case tomato = "토마토"
+    case walnut = "호두"
+    case mussel = "홍합"
+}
