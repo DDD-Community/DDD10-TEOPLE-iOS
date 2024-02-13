31e8933a78b097d7baed2c2ed5f2aebcf8e9b019
Juny0110 <wnsdyds403@naver.com>
2024-02-13 14:55:53 +0900

    Design #16: CustomButton 추가

--- Umat/Projects/Shared/DesignSystem/Sources/Component/Buttons/CustomButton.swift
+++ Umat/Projects/Shared/DesignSystem/Sources/Component/Buttons/CustomButton.swift
@@ -2,71 +2,61 @@
 //  CustomButton.swift
 //  DesignSystem
 //
-//  Created by 지준용 on 2/8/24.
+//  Created by 지준용 on 2/11/24.
 //  Copyright © 2024 KYUNG MIN CHOI. All rights reserved.
 //
 
 import SwiftUI
 
-
-public struct CustomButton: View {
-    
-    // MARK: - Properties
-    private let leadingIcon: Icons?
-    private let iconColor: Color?
+public struct CustomButton<F: ShapeStyle, B: ShapeStyle>: View {
+    private let icon: Icons?
+    private let iconSize: CGFloat
     private let text: String
-    private let buttonSize: ButtonSize
-    private let buttonState: ButtonState
+    private let foregroundStyle: F
+    private let background: B
+    private let strokeColor: Color
+    private let height: CGFloat
     private let maxWidth: CGFloat
+    private let cornerRadius: CGFloat
     private let action: () -> Void
-    private let cornerRadius: CGFloat = 8
     
-    // MARK: - Init
-
-    public init(leadingIcon: Icons? = nil,
-                iconColor: Color? = nil,
+    public init(icon: Icons? = nil,
+                iconSize: CGFloat = 18,
                 text: String,
-                buttonSize: ButtonSize,
-                buttonState: ButtonState,
+                foregroundStyle: F,
+                background: B,
+                strokeColor: Color = .clear,
+                height: CGFloat = 50,
                 maxWidth: CGFloat = .infinity,
+                cornerRadius: CGFloat = 8,
                 action: @escaping () -> Void) {
-        self.leadingIcon = leadingIcon
-        self.iconColor = iconColor
+        self.icon = icon
+        self.iconSize = iconSize
         self.text = text
-        self.buttonSize = buttonSize
-        self.buttonState = buttonState
+        self.foregroundStyle = foregroundStyle
+        self.background = background
+        self.strokeColor = strokeColor
+        self.height = height
         self.maxWidth = maxWidth
+        self.cornerRadius = cornerRadius
         self.action = action
     }
     
-    // MARK: - Views
     public var body: some View {
         Button(action: {
             action()
         }, label: {
-            HStack(spacing: 6) {
-                if let icon = leadingIcon?.image {
-                    icon
-                        .resizable()
-                        .renderingMode(.template)
-                        .scaledToFit()
-                        .foregroundStyle(iconColor ?? buttonState.foregroundStyle)
-                        .frame(width: buttonSize.icon,
-                               height: buttonSize.icon)
-                }
-                
-                Text(text)
-                    .fontModifier(buttonSize.font)
-            }
-            .padding(buttonSize.insets)
-            .frame(maxWidth: maxWidth)
-            .foregroundStyle(buttonState.foregroundStyle)
-            .background(buttonState.background)
-            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
-            .overlay(RoundedRectangle(cornerRadius: cornerRadius)
-                .stroke(buttonState.stroke)
-            )
+            CustomLabel(icon: icon,
+                        iconSize: iconSize,
+                        text: text,
+                        foregroundStyle: foregroundStyle,
+                        background: background,
+                        strokeColor: strokeColor,
+                        height: height,
+                        maxWidth: maxWidth,
+                        cornerRadius: cornerRadius)
         })
-        .padding(.horizontal, 24)
     }
 }
+
+
