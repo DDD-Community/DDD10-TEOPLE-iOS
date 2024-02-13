b75c529a7c7e598b0a76b1c95510b5d70b05616a
Juny0110 <wnsdyds403@naver.com>
2024-02-13 15:02:19 +0900

    Refactor #16: TextInput argument 수정, 내부 method 삭제
    - header를 guidanceText로 수정
    - fontmodifier 기본값 수정
      - guidanceText: lb20 -> lb12
      - placeholder: pb16 -> pr16
    - TextInput의 범용성을 위해 onChange modifier 및 내부 메서드 삭제

--- Umat/Projects/Shared/DesignSystem/Sources/Component/TextInput/TextInput.swift
+++ Umat/Projects/Shared/DesignSystem/Sources/Component/TextInput/TextInput.swift
@@ -12,7 +12,7 @@ import SwiftUI
 public struct TextInput<T: Hashable>: View {
     
     // MARK: - Properties
-    private let header: String
+    private let guidanceText: String
     @Binding private var text: String
     private let placeholder: String
     @Binding private var supportingText: String
@@ -21,14 +21,14 @@ public struct TextInput<T: Hashable>: View {
     @Binding private var stateColor: Color?
     
     // MARK: - Init
-    public init(header: String,
+    public init(guidanceText: String,
                 text: Binding<String>,
                 placeholder: String,
                 supportingText: Binding<String>,
                 focusState: FocusState<T>.Binding,
                 focusValue: T = true,
                 stateColor: Binding<Color?>) {
-        self.header = header
+        self.guidanceText = guidanceText
         self._text = text
         self.placeholder = placeholder
         self._supportingText = supportingText
@@ -40,29 +40,17 @@ public struct TextInput<T: Hashable>: View {
     // MARK: - Views
     public var body: some View {
         VStack(alignment: .leading) {
-            Text(header)
-                .fontModifier(.lb20)
+            Text(guidanceText)
+                .fontModifier(.lb12)
                 .foregroundStyle(stateColor ?? .colors(.gray800))
                 .padding(.bottom, 10)
             
             TextField(placeholder, text: $text)
-                .fontModifier(.pb16)
+                .fontModifier(.pr16)
                 .foregroundStyle(.colors(.gray900))
                 .focused(focusState, equals: focusValue)
                 .textInputAutocapitalization(.never)
                 .autocorrectionDisabled()
-                .onChange(of: text) { oldValue, newValue in
-                    switch textInputState(oldValue, newValue) {
-                    case .enable:
-                        stateColor = nil
-                    case .disable:
-                        stateColor = .colors(.gray400)
-                    case .error:
-                        stateColor = .colors(.error)
-                    case .activate:
-                        stateColor = .colors(.blue600)
-                    }
-                }
             
             Rectangle()
                 .frame(height: 1)
@@ -74,25 +62,6 @@ public struct TextInput<T: Hashable>: View {
                 .frame(maxWidth: .infinity, alignment: .trailing)
                 .foregroundStyle(stateColor ?? .colors(.gray600))
         }
-        .padding(.horizontal, 24)
-    }
-    
-    // MARK: - Methods
-    private func textInputState(_ oldValue: String, _ newValue: String) -> TextInputState {
-        if !isValidateTextInput(oldValue, newValue) {
-            return .error
-        } else {
-            return .enable
-        }
-    }
-    
-    private func isValidateTextInput(_ oldValue: String, _ newValue: String) -> Bool {
-        if (!oldValue.isEmpty && newValue.isEmpty) ||
-            newValue.count > 6 ||
-            !newValue.isKoreanLanguage() {
-            return false
-        }
-        return true
     }
 }
 
