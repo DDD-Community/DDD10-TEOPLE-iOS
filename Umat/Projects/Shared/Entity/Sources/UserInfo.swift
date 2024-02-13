5391ef8f77b3265702727c0e47419c273be493cc
Gordon Choi <thefirate@gmail.com>
2024-02-13 23:02:29 +0900

    Feat #15: 커플 모델 작성 및 모델에 관한 주석 추가

--- Umat/Projects/Shared/Entity/Sources/UserInfo.swift
+++ Umat/Projects/Shared/Entity/Sources/UserInfo.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+/// 사용자 한 명에 대한 정보입니다.
+/// id값은 바뀌면 안 되므로 let입니다.
+/// 이름이나 알러지 품목은 바뀔 수 있으므로 var로 하고, 수정 메서드를 작성했습니다.
 public struct UserInfo {
     private let id: String
     public private(set) var name: String
@@ -21,13 +24,16 @@ public struct UserInfo {
     
     public mutating func changeName(name newName: String) {
         name = newName
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
     }
     
     public mutating func changeAllergicFoods(foods newAllergicFoods: [AllergicFood]) {
         allergicFoods = newAllergicFoods
+        // TODO: 서버에 반영하는 과정도 필요합니다 (feature 단에서 수행할 가능성이 높습니다)
     }
 }
 
+/// 식약처에서 제공하는 알러지 품목에 관한 목록입니다.
 public enum AllergicFood: String, CaseIterable {
     case crab = "게"
     case mackerel = "고등어"
