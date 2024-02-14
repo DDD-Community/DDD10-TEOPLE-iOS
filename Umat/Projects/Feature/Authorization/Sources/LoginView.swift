d0a33477c21cb93b37fc6421d21ebbfcab850644
Juny0110 <wnsdyds403@naver.com>
2024-02-14 17:43:05 +0900

    Feat #14: LoginMethod extension 추가
    - 로그인 수단별 Icon, text, color 등을 반영하기 위한 extension 추가

---
+++ Umat/Projects/Feature/Authorization/Sources/LoginView.swift
@@ -0,0 +1,39 @@
+
+// MARK: - LoginMethod
+extension LoginMethod {
+    var icon: Icons? {
+        switch self {
+        case .kakao:
+            return .kakao
+        case .apple:
+            return .apple
+        }
+    }
+    
+    var text: String {
+        switch self {
+        case .kakao:
+            return "카카오로 로그인하기"
+        case .apple:
+            return "Apple로 로그인하기"
+        }
+    }
+    
+    var foregroundStyle: Color {
+        switch self {
+        case .kakao:
+            return .init(hex: "#262626")
+        case .apple:
+            return .white
+        }
+    }
+    
+    var background: Color {
+        switch self {
+        case .kakao:
+            return .init(hex: "#FEE500")
+        case .apple:
+            return .colors(.gray950)
+        }
+    }
+}
