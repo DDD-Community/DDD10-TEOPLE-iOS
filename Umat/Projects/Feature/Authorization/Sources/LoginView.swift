bb2a3179739893498db304144e24ed3a2afe3291
Juny0110 <wnsdyds403@naver.com>
2024-02-14 17:43:35 +0900

    Design #14: LoginView UI 추가

--- Umat/Projects/Feature/Authorization/Sources/LoginView.swift
+++ Umat/Projects/Feature/Authorization/Sources/LoginView.swift
@@ -1,3 +1,94 @@
+//
+//  LoginView.swift
+//  Authorization
+//
+//  Created by 지준용 on 1/27/24.
+//  Copyright © 2024 KYUNG MIN CHOI. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+import Entity
+
+public struct LoginView: View {
+    
+    // MARK: - Init
+    public init() {}
+    
+    // MARK: - Views
+    public var body: some View {
+        BaseView {
+            header()
+        } content: {
+            buttons()
+        } footer: {
+            footer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+}
+
+fileprivate extension LoginView {
+    @ViewBuilder
+    func header() -> some View {
+        Color.colors(.gray200)
+            .frame(height: 480)
+            .ignoresSafeArea()
+            .overlay {
+                VStack {
+                    Image.icons(.ic_pin)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.gradient())
+                        .frame(width: 137, height: 137)
+                        .padding(.top, 145)
+                    
+                    Text("맛나")
+                        .font(.pretendard(.bold, size: 48))
+                        .padding(.bottom, 73)
+                }
+            }
+    }
+    
+    @ViewBuilder
+    func buttons() -> some View {
+        Spacer()
+        
+        VStack(spacing: 16) {
+            ForEach(LoginMethod.allCases, id: \.self) { method in
+                CustomButton(icon: method.icon,
+                             text: method.text,
+                             foregroundStyle: method.foregroundStyle,
+                             background: method.background) {
+                    // TODO: 로그인 플로우
+                    print(method.text)
+                }
+            }
+            
+            NavigationLink {
+                // TODO: 지도로 이동하기
+                EmptyView()
+            } label: {
+                Text("둘러보기")
+                    .pretendard(.semiBold14)
+                    .foregroundStyle(.colors(.gray600))
+                    .frame(height: 34)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        .padding(.bottom, 16)
+        .padding(.horizontal, 24)
+    }
+    
+    @ViewBuilder
+    func footer() -> some View {
+        Text("로그인 시 개인정보처리방침과 이용약관에 동의하게 됩니다")
+            .pretendard(.medium12)
+            .foregroundStyle(.colors(.gray400))
+            .padding(.bottom, 54)
+            .padding(.horizontal, 24)
+    }
+}
 
 // MARK: - LoginMethod
 extension LoginMethod {
