395b3dc79b315c99e081b20a10b1f833b9fdddcf
Juny0110 <wnsdyds403@naver.com>
2024-02-14 23:23:20 +0900

    Design #14: BaseView 수정사항 적용

--- Umat/Projects/Feature/Authorization/Sources/LoginView.swift
+++ Umat/Projects/Feature/Authorization/Sources/LoginView.swift
@@ -17,7 +17,7 @@ public struct LoginView: View {
     
     // MARK: - Views
     public var body: some View {
-        BaseView(isContentCentered: true) {
+        BaseView(isContentCentered: false) {
             header()
         } content: {
             buttons()
