fbbe42764dbb323d24aa59098969eac5ee513019
Gordon Choi <thefirate@gmail.com>
2024-02-17 17:56:24 +0900

    Design #21: 임시 환경설정 뷰 정의

---
+++ Umat/Projects/Feature/MyPage/Sources/View/ConfigView.swift
@@ -0,0 +1,40 @@
+//
+//  ConfigView.swift
+//  MyPage
+//
+//  Created by Gordon Choi on 2/17/24.
+//  Copyright © 2024 KYUNG MIN CHOI. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ConfigView: View {
+    public var body: some View {
+        NavigationStack {
+            List {
+                ForEach(ConfigItems.allCases, id: \.self) { item in
+                    NavigationLink(destination: Text(item.rawValue)) {
+                        Text(item.rawValue)
+                    }
+                    .listRowSeparator(.hidden)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("환경설정")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    public init() { }
+}
+
+enum ConfigItems: String, CaseIterable {
+    case versionInfo = "버전 정보"
+    case termsOfUse = "이용 약관"
+    case logout = "로그아웃"
+    case connect = "커플 연결"
+}
+
+#Preview {
+    ConfigView()
+}
