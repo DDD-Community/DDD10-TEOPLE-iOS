ffc6656071aa0ec533e077b46aef19206db8c1c3
Juny0110 <wnsdyds403@naver.com>
2024-02-25 15:10:48 +0900

    Feat #30: Place 모델 내 address 추가, protocol 수정

--- Umat/Projects/Shared/Entity/Sources/Place.swift
+++ Umat/Projects/Shared/Entity/Sources/Place.swift
@@ -11,21 +11,31 @@ import Foundation
 /// 장소 모델입니다.
 /// 구글 장소 api에 요청한 결과를 토대로 가져옵니다.
 /// CoreLocation을 아직 사용하지 않습니다. 임시로 위치 타입을 정해 두었습니다.
-public struct Place: Equatable {
+public struct Place: Hashable, Codable {
     public let name: String
+    public let address: String
     public let openingHour: String
     public let closingHour: String
     public let grade: Double
     public let location: Location
     public let imageURL: URL?
+    // TODO: 요일에 대한 정보 필요
     
-    public struct Location: Equatable {
+    public struct Location: Hashable, Codable {
         public let latitude: Double
         public let longitude: Double
     }
     
-    public init(name: String, openingHour: String, closingHour: String, grade: Double, latitude: Double, longitude: Double, imageURL: URL?) {
+    public init(name: String, 
+                address: String,
+                openingHour: String,
+                closingHour: String,
+                grade: Double,
+                latitude: Double,
+                longitude: Double,
+                imageURL: URL?) {
         self.name = name
+        self.address = address
         self.openingHour = openingHour
         self.closingHour = closingHour
         self.grade = grade
