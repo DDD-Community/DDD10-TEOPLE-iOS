4f64e284969795985b3cb285fdf3e191d370b525
Gordon Choi <thefirate@gmail.com>
2024-02-28 02:19:32 +0900

    Feat #31: 토글할 수 있는 주변 지역 도형 마킹 구현

--- Umat/Projects/Core/Maps/Sources/NaverMapView.swift
+++ Umat/Projects/Core/Maps/Sources/NaverMapView.swift
@@ -25,49 +25,32 @@ public struct NaverMapView: UIViewRepresentable {
     }
     
     public func makeUIView(context: Context) -> NMFNaverMapView {
-        let view = NMFNaverMapView()
-        
-        view.showZoomControls = false
-        view.showLocationButton = true
-        
-        view.mapView.positionMode = .direction
-        view.mapView.zoomLevel = 17
-        view.mapView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 200, right: 0)
-        
-        return view
+        context.coordinator.removeAllItems()
+        return context.coordinator.makeMapView()
     }
     
     public func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        guard let location else { return }
-        
-        let newLocation = NMGLatLng(lat: location.latitude, lng: location.longitude)
-        let newFocus = NMFCameraUpdate(scrollTo: newLocation)
-        newFocus.animation = .fly
-        newFocus.animationDuration = 1
-        uiView.mapView.moveCamera(newFocus)
+//        guard let location else { return }
+//        
+//        let newLocation = NMGLatLng(lat: location.latitude, lng: location.longitude)
+//        let newFocus = NMFCameraUpdate(scrollTo: newLocation)
+//        newFocus.animation = .fly
+//        newFocus.animationDuration = 1
+//        uiView.mapView.moveCamera(newFocus)
         
         if isLocalAreaMarked {
-            Logger.print("Draw circle")
-            let mapView = uiView.mapView
-            let projection = mapView.projection
-            
-            let leftCoord = projection.latlng(from: CGPoint(x: 0, y: uiView.bounds.height / 2)).toCLLocation()
-            let rightCoord = projection.latlng(from: CGPoint(x: uiView.bounds.width, y: uiView.bounds.height / 2)).toCLLocation()
-            let distance = rightCoord.distance(from: leftCoord)
-            
-            let center = mapView.cameraPosition.target
-            let radius = distance / 2
-            let overlay = NMFCircleOverlay(center, radius: radius)
-            
-            overlay.fillColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)
-            overlay.mapView = mapView
+            context.coordinator.createCircle()
             
             // 오버레이 원 안에 있는 핀 갯수 파악하기
         } else {
-            // 원형 오버레이 지우기
+            context.coordinator.deleteCircle()
             
             // 전체 갯수로 수정하기
         }
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator.shared
     }
 }
 
